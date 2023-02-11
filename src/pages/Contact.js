ebeb8bd340d0ba493a169d4f1308403f5aad86ee
Christopher Dirksen <chrisdirksen024@outlook.com>
2023-02-11 14:42:53 -0500

    commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -11,6 +11,8 @@ export default function Contact () {
             <p><a href='https://www.linkedin.com/in/christopher-dirksen-726035263' target='_blank' >linkedin.com/in/christopher-dirksen-726035263</a></p>
             <p><h2>GitHub</h2></p>
             <p><a href='https://www.github.com/chrispyrice024' target='_blank'>ChrispyRice024</a></p>
+            <p><h2>This Projects Repo</h2></p>
+            <p><a href='https://github.com/ChrispyRice024/Team_Selector' target='_blank'>github.com/ChrispyRice024/Team_Selector</a></p>
         </div>
     )
 }
